e25d66cf1c11933b49b4192bc4b510f5f678eb07
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-05-22 17:07:26 +0200

    seeds for a user and a kennel

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,13 @@
-# Kennel.create!(city:"Lyon", address:"24 rue de la Fleur", description:"I will take care of your dog")
+user = User.create(
+  email: 'test_1@gmail.com',
+  password: 'password',
+  first_name: 'Julie',
+  last_name: 'Tartempion'
+)
+
+Kennel.create(
+  user: user,
+  city:"Lyon",
+  address:"24 rue de la Fleur",
+  description:"I will take care of your dog"
+)
