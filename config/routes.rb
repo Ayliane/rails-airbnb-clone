ab22f50ade302904e1d8bec5980e9ac2c8b10771
Guillaume Grand <guillaume.grand80@orange.fr>
2017-05-23 16:51:31 +0200

    ajoue des routes pour edit et update un user/id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
+  resources :user, only: [:edit, :update]
   resources :kennels do
     resources :bookings, only: [:new, :create]
   end
