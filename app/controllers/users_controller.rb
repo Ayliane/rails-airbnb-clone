eb84b4f54922d55269d27c004ea035b74494a24f
Guillaume Grand <guillaume.grand80@orange.fr>
2017-05-23 17:39:32 +0200

    vue users/index up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.new(user_params)
+
+      if @user.save
+        redirect_to kennels_path
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :email, :photos)
+  end
 end
