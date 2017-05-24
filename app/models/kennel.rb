fe43e0576c39bea31c7792d763017f9ce3ed3aa7
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-05-24 17:06:22 +0200

    modify kennel model

--- app/models/kennel.rb
+++ app/models/kennel.rb
@@ -7,4 +7,7 @@ class Kennel < ApplicationRecord
   validates :address, presence: true
   validates :description, presence: true
   validates :city, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
