7d3b2598ac22e5f7b12b41e8f627f76af5d68715
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-05-24 13:56:37 +0200

    remove wait page & add message to booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523104245) do
+ActiveRecord::Schema.define(version: 20170524111807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170523104245) do
     t.integer  "kennel_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.text     "message"
     t.index ["kennel_id"], name: "index_bookings_on_kennel_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
