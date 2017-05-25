b38402295c0ccdc08a0cee44c9346a6f81a720ff
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-05-25 10:06:33 +0200

    Correcting routes for index bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update] do
+    resources :bookings, only: [:index]
+  end
   resources :kennels do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:index, :show, :edit, :update]
+  resources :bookings, only: [:show, :edit, :update]
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
