162b79b99a94ef51e59c4a588865ba9a4a0802f5
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-05-25 17:05:05 +0200

    Confirm your fuckin' reservation

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :kennels do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:show, :edit, :update]
+  resources :bookings, only: [:show, :edit, :update] do
+      put 'toggle'
+    end
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
