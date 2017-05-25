74c1ac98d88ad59bed29bf75af92470ab71476a4
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-05-25 17:10:28 +0200

    user picture large show profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,8 @@ class User < ApplicationRecord
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
-    user_params[:facebook_picture_url] = auth.info.image
+    large_img = auth.info.image.gsub(/\bsquare\b/, 'large')
+    user_params[:facebook_picture_url] = large_img
     user_params[:token] = auth.credentials.token
     user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
     user_params = user_params.to_h
